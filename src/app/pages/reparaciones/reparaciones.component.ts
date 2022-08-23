fb2902d2915a128b4dbfb637a233d8d24bdd96e9
Hernan Pedrino <hernanpedrino@gmail.com>
2022-08-23 10:43:43 -0300

    Edicion reparacion

--- src/app/pages/reparaciones/reparaciones.component.ts
+++ src/app/pages/reparaciones/reparaciones.component.ts
@@ -12,6 +12,12 @@ export class ReparacionesComponent implements OnInit {
   public finishedRepairs: any[] = [];
   public afiladosActivos: any[] = [];
   public afiladosTerminados: any[] = [];
+  public edicion: boolean = false;
+  public nombre: string | undefined;
+  public apellido: string | undefined;
+  public fecha: Date | undefined;
+
+
   constructor(private datosService: DatosService) { }
 
   ngOnInit(): void {
@@ -19,6 +25,8 @@ export class ReparacionesComponent implements OnInit {
   }
   reparaciones(){
     this.datosService.getReparaciones().subscribe((resp:any)=>{
+      console.log(resp);
+      
       let tempArr = []
       tempArr = resp.repairs;
       const filtroRepActivas = tempArr.filter((active:any) => active.reparacionTerminada == false && active.reparacion == 'reparacion');
@@ -30,6 +38,14 @@ export class ReparacionesComponent implements OnInit {
       const filtroAfiladosTerminados = tempArr.filter((active:any) => active.reparacionTerminada == true && active.reparacion == 'afilado');
       this.afiladosTerminados = filtroAfiladosTerminados;
     });
+  }
+  editar(i:number){
+    console.log(this.activeRepairs[i]);
+    this.edicion = true;
+    this.nombre = this.activeRepairs[i].usuario.nombre;
+    this.apellido = this.activeRepairs[i].usuario.apellido;
+    this.fecha = this.activeRepairs[i].fechaRecepcion;
+    
   }
   
 }
